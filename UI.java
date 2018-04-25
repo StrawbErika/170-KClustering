a9a3509faf2960bee58ce46d85ab22309db447b6
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-04-26 02:18:01 +0800

    +prints out the centroids & points on appropriate GUI, *adjusted Size

--- UI.java
+++ UI.java
@@ -9,7 +9,9 @@ import javax.imageio.*;
 public class UI {
     private JFrame frame;
     public Kcluster cluster;
+    public int turn;
     public UI() {
+            this.turn = 1;
             this.cluster = new Kcluster();
             this.initializeUI();
     }
@@ -18,7 +20,7 @@ public class UI {
         JLabel stats02= new JLabel();
         JFrame frame = new JFrame("Ham & Spam");//creates a frame/window; javax.swing.JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(2000, 900));//sets initial size resolution of the frame...; java.awt.Dimensio
+        frame.setPreferredSize(new Dimension(2000, 700));//sets initial size resolution of the frame...; java.awt.Dimensio
 
         Container con = frame.getContentPane();
         JPanel contentPanel= new JPanel();
@@ -33,8 +35,42 @@ public class UI {
         JButton information= new JButton("Select input text");
 
         Graph graph= new Graph();
-        graph.setPreferredSize(new Dimension(750,750));
+        graph.setPreferredSize(new Dimension(750,500));
         graph.setBackground(Color.WHITE);
+
+        information.setPreferredSize(new Dimension(200,50));
+
+        tabPanel.add(information);
+        tabPanel.add(graph);
+
+        contentPanel.setLayout(new CardLayout());
+        CardLayout cardLayout= (CardLayout) contentPanel.getLayout();
+
+        JPanel hamPanel= new JPanel();
+        hamPanel.setLayout(new FlowLayout());
+        hamPanel.setPreferredSize(new Dimension(250,550));
+
+        JPanel filler = new JPanel();
+        filler.setPreferredSize(new Dimension(700,50));
+
+        JPanel pointClass= new JPanel();
+        pointClass.setPreferredSize(new Dimension(800,50));
+        pointClass.setBackground(Color.GRAY);
+        pointClass.setLayout(new GridLayout(1,2));
+        JLabel point = new JLabel("Point");
+        JLabel className = new JLabel("Centroid");
+        pointClass.add(point);
+        pointClass.add(className);
+
+        JPanel pointClassPanel= new JPanel();
+        pointClassPanel.setPreferredSize(new Dimension(800,200));
+        pointClassPanel.setBackground(Color.WHITE);
+        pointClassPanel.setLayout(new GridLayout(1,2));
+        JTextArea pointPanel = new JTextArea();
+        JTextArea classPanel = new JTextArea();
+        pointClassPanel.add(pointPanel);
+        pointClassPanel.add(classPanel);
+
 
         information.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
@@ -49,53 +85,51 @@ public class UI {
 
                     graph.k = cluster.k;
                     if(cluster.isTwo){
-                        for(int i = 0; i < cluster.iterations.get(1).size(); i++){
-                            cluster.iterations.get(1).get(i).print();
-                            graph.centroids.add(cluster.iterations.get(1).get(i));
+                        for(int i = 0; i < cluster.iterations.get(turn).size(); i++){
+                            cluster.iterations.get(turn).get(i).print();
+                            graph.centroids.add(cluster.iterations.get(turn).get(i));
                         }
                         graph.repaint();
-                    // }
-                }
+
+                        for(int i = 0; i < cluster.iterations.get(turn).size(); i++){
+                            Centroids thisCentroid = cluster.iterations.get(turn).get(i);
+                            for(int k = 0; k < thisCentroid.vectors.size(); k++){
+                                ArrayList<Double> thisVList = thisCentroid.vectors.get(k).list;
+                                ArrayList<Double> thisCList = thisCentroid.list;
+
+                                String Zero = Double.toString(thisVList.get(0));
+                                String One = Double.toString(thisVList.get(1));
+                                String vectorPoint = Zero + " , "+One +"\n" ;
+
+                                String Zero2 = Double.toString(thisCList.get(0));
+                                String One2 = Double.toString(thisCList.get(1));
+                                String centroidPoint = Zero2 + " , "+One2 +"\n" ;
+
+                                pointPanel.append(vectorPoint);
+                                classPanel.append(centroidPoint);
+                            }
+                        }
+
+                    }
+
                 frame.requestFocus();
                 }
         });
 
-        information.setPreferredSize(new Dimension(200,50));
 
-
-        tabPanel.add(information);
-        tabPanel.add(graph);
-
-
-        contentPanel.setLayout(new CardLayout());
-        CardLayout cardLayout= (CardLayout) contentPanel.getLayout();
-
-        //panel 1
-
-        JPanel hamPanel= new JPanel();
-        hamPanel.setLayout(new FlowLayout());
-        hamPanel.setPreferredSize(new Dimension(250,550));
-
-        JPanel filler = new JPanel();
-        filler.setPreferredSize(new Dimension(700,50));
-
-        JPanel pointClass= new JPanel();
-        pointClass.setPreferredSize(new Dimension(550,50));
-        pointClass.setBackground(Color.GRAY);
-        pointClass.setLayout(new GridLayout(1,2));
-        JLabel point = new JLabel("Point");
-        JLabel className = new JLabel("Centroid");
-        pointClass.add(point);
-        pointClass.add(className);
-
-        JPanel pointClassPanel= new JPanel();
-        pointClassPanel.setPreferredSize(new Dimension(550,190));
-        pointClassPanel.setBackground(Color.WHITE);
-        pointClassPanel.setLayout(new GridLayout(1,2));
-        JLabel pointPanel = new JLabel();
-        JLabel classPanel = new JLabel();
-        pointClassPanel.add(pointPanel);
-        pointClassPanel.add(classPanel);
+    //   classify.addActionListener(new ActionListener(){
+    //     public void actionPerformed(ActionEvent e){
+    //       String data01= wordPanel.getText();
+    //       pointPanel.setText(data01);
+    //       nearest.getX(data01);
+    //       nearest.getAllDistance();
+    //       nearest.getKNearest();
+    //       nearest.getC();
+    //       classPanel.setText(classData);
+    //       nearest.saveFile();
+          
+    //     }
+    //   });
 
         hamPanel.add(filler);
         hamPanel.add(pointClass);
